0be5b1dbbf2ea0fa725cd706fdf158e5eee5bafa
Zhanhao Zhang <zz564@alpha1.cm.cluster>
2026-01-31 17:41:02 -0500

    add results

---
+++ SingleAgent-Stage2/submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=empire_gpu
+#SBATCH --partition=cornell
+#SBATCH --account=cornell
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-node=1  # Request 1 GPU (max 8)
+#SBATCH --time=24:00:00     # Time limit hrs:min:sec
+#SBATCH --output=job_%j.out
+
+# Run your command
+srun python3 SingleAgentPipe.py
+
